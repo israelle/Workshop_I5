ec1d68bc0682f8eb834b7a5c705ccb960361ddcd
israelle <israelle.lagier972@gmail.com>
2018-11-27 13:44:44 +0100

    ajout de composent

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { OAuthModule } from "angular-oauth2-oidc";
+import { HomeComponent } from './home/home.component';
+import { AuthGuard } from "./shared/auth/auth.guard.service";
+import { EditComponent } from "./edit/edit.component";
 
+const appRoutes: Routes = [
+//    { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
+    { path: 'edit/:id', component: EditComponent, canActivate: [AuthGuard] },
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      HttpClientModule,
+      OAuthModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
