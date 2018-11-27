1f5584c7f7ac101009870948b5d3f228facc40cc
israelle <israelle.lagier972@gmail.com>
2018-11-28 00:26:42 +0100

    header search bar

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+    isLogin: boolean;
   constructor() { }
 
   ngOnInit() {
+    this.isLogin= false;
   }
 
 }
